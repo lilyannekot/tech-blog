fa8c09840b057519fc7cc0cbea7986fefd82c1ba
lilys tacos <lilyannek@gmail.com>
2022-08-02 17:45:45 -0700

    necessary additions to server.js

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const routes = require("./controllers");
 const sequelize = require("./config/connection.js");
+const path = require("path");
+
+const helpers = require("./utils/helpers");
+
 const exphbs = require("express-handlebars");
+const hbs = exphbs.create({ helpers });
+
 const session = require("express-session");
 
 const app = express();
@@ -21,9 +27,14 @@ const sess = {
 };
 
 app.use(session(sess));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 app.use(routes);
 
 // Turn on connection to db and server
